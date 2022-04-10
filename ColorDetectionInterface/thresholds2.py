86c9073f6d8f446fc60adff8ba4fb0871c6c1525
ivpcl <65985936+ivpcl@users.noreply.github.com>
2022-04-10 15:57:41 -0600

    Update thresholds2.py

--- ColorDetectionInterface/thresholds2.py
+++ ColorDetectionInterface/thresholds2.py
@@ -139,7 +139,7 @@ class threshold:
             plt.legend()
             plt.xlabel('Pixel values', fontsize=18)
             plt.ylabel('Number of occurrences',fontsize=18)
-            plt.title(color[i], fontsize = 24)
+            plt.title(color[i], fontsize = 2)
             plt.xlim([0,256])
             plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
             plt.title(col)
